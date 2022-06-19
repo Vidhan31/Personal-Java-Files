d07fa058a5c6ac4e8e791494fd3edd1cbd996c31
Vidhan <rautvidhan31@gmail.com>
2022-06-19 18:00:17 +0530

    Specialized generics class.

--- src/Generics/Main.java
+++ src/Generics/Main.java
@@ -22,22 +22,47 @@ class JavaGenerics<T> {
     }
 }
 
+class SpecializedGenericTypes<T extends Animal> {
+    //class SpecializedGenericTypes<T extends Animal> -> makes only Animal available in the class
+
+    T species;
+
+    public SpecializedGenericTypes(T species) {
+        this.species = species;
+    }
+
+    public void printObject() {
+        species.makeNoise();
+    }
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
+
         //argument passed to the type parameter must be a reference type. primitive arrays are allowed
         // as they themselves are reference types.
         JavaGenerics<Integer> genObj1 = new JavaGenerics<>(34);
         genObj1.printValues();
+
         //Type safety: Specifying the type beforehand helps us locate the problem at compile time rather than runtime.
+        //here <T> or <Double> is a bound.
         JavaGenerics<Double> genObj2 = new JavaGenerics<>(33.43223423);
         genObj2.printValues();
+
         //elements individual type casting
         JavaGenerics<String> genObj3 = new JavaGenerics<>("Generics!");
         genObj3.printValues();
-        genericMethod("Hello generic method!");
-        //Even if all above are of Type T, that doesn't mean genObj1 = genObj2 = genObj3.
+        genericMethod("Hello generic method!"); //Even if all above are of Type T, that doesn't mean genObj1 = genObj2 = genObj3.
         //They are still different types of T. They are different references.
+
+
+        //Specialized generics
+        SpecializedGenericTypes<Animal> animal = new SpecializedGenericTypes<>(new Animal());
+        animal.printObject(); //prints Animal methods even if overridden
+        SpecializedGenericTypes<Dog> dog = new SpecializedGenericTypes<>(new Dog());
+        dog.printObject(); //print dog methods as it is subclass of animal
     }
 
     public static <T> void genericMethod(T varY) {
