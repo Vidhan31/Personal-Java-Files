2084513680febb9fa3c216615cfc3c0ca7418957
Vidhan <rautvidhan31@gmail.com>
2022-06-19 14:41:53 +0530

    added basic generic type class

--- src/Generics/Main.java
+++ src/Generics/Main.java
@@ -1,4 +1,25 @@
 package Generics;
 
+class JavaGenerics<T> {
+
+    T varX;
+
+    public JavaGenerics(T varX) {
+        this.varX = varX;
+    }
+
+    public void printValues() {
+        System.out.println("Value : " + varX);
+    }
+}
+
 public class Main {
+    public static void main(String[] args) {
+        JavaGenerics<Integer> genObj1 = new JavaGenerics<>(34);
+        genObj1.printValues();
+        JavaGenerics<Double> genObj2 = new JavaGenerics<>(33.43223423);
+        genObj2.printValues();
+        JavaGenerics<String> genObj3 = new JavaGenerics<>("Generics!");
+        genObj3.printValues();
+    }
 }
