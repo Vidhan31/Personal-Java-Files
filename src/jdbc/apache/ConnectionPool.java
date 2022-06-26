5b04737f46f44a4df38bb5c13a21343c9d782d0d
Vidhan <rautvidhan31@gmail.com>
2022-06-26 16:49:33 +0530

    Added double check lock singleton pattern for thread safe program.

--- src/jdbc/apache/ConnectionPool.java
+++ src/jdbc/apache/ConnectionPool.java
@@ -6,11 +6,12 @@ import javax.sql.DataSource;
 import java.io.FileInputStream;
 import java.util.Properties;
 
-public class ConnectionPool {
+public class ConnectionPool { //singleton pattern - lazy instantiation.
 
-    private static BasicDataSource dataSource = null;
+    private volatile static BasicDataSource dataSource = null;
 
-    private ConnectionPool() {}
+    private ConnectionPool() {
+    }
 
     synchronized public static DataSource getDataSource() throws Exception {
 
@@ -18,11 +19,14 @@ public class ConnectionPool {
             Properties property = new Properties();
             property.load(new FileInputStream("src/jdbc/apache/database.properties"));
 
-            if (dataSource == null) {
-                System.out.println("new instance of datasource");
-                dataSource = new BasicDataSource();
+            if (dataSource == null) { //double check lock
+                synchronized (ConnectionPool.class) {
+                    if (dataSource == null) {
+                        System.out.println("new instance of datasource");
+                        dataSource = new BasicDataSource();
+                    }
+                }
             }
-
             dataSource.setUrl(property.getProperty("DB_URL"));
             dataSource.setUsername(property.getProperty("DB_USERNAME"));
             dataSource.setPassword(property.getProperty("DB_PASSWORD"));
